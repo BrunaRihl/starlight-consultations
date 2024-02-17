4690f41380dfdbfc54208417933581ca68df1aa9
BrunaRihl <brunarihl@gmail.com>
2024-02-17 15:34:28 +0000

    feat: create superuser and enter data into the database

--- starlight/settings.py
+++ starlight/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b&jol)@(e5@z03_+=(ro!kgp#$e#i6cpme316kfvv_aui@t4n9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [    
     '.herokuapp.com',
