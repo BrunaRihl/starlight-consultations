5f095bb6620912d8d631874d76161eff69da6ba5
BrunaRihl <brunarihl@gmail.com>
2024-02-17 15:59:46 +0000

    feat: enhanced security, updated SECRET_KEY, add to env.py

--- starlight/settings.py
+++ starlight/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b&jol)@(e5@z03_+=(ro!kgp#$e#i6cpme316kfvv_aui@t4n9'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
